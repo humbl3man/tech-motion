d621bb6782aedf9beda0306519bf69466e415273
Konstantin <humbl3dev@gmail.com>
2023-07-06 00:30:40 -0700

    cart show subtotal and checkout button

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -6,68 +6,85 @@
 	export let data;
 	let deleteSkus: Array<number | string> = [];
 
-	$: ({ cart } = data);
+	$: cartItems = data.cartItems ?? [];
+	$: subtotal = cartItems.reduce((total, item) => {
+		total += item.quantity * item.product?.price;
+		return total;
+	}, 0);
 </script>
 
-<div class="mx-auto max-w-screen-md px-4">
+<div class="mx-auto mb-16 max-w-screen-md px-4">
 	<h1 class="mb-8 text-3xl">Your Shopping Cart</h1>
-	{#if cart?.items && cart.items.length > 0}
-		{#each cart.items as item}
-			<div class="grid grid-cols-[200px_1fr] gap-8 border-t-slate-100 py-8">
-				<div>
-					<a href="/product/{item.product.sku}">
-						<img
-							class="aspect-square rounded-lg object-cover"
-							src={item.product.image || `/images/product/${item.product.sku}.jpg`}
-							alt={item.product.name}
-							loading="lazy"
-						/>
-					</a>
+	{#each cartItems as item}
+		<div class="grid grid-cols-[200px_1fr] gap-8 border-t-slate-100 py-4">
+			<div>
+				<a href="/product/{item.product.sku}">
+					<img
+						class="aspect-square rounded-lg object-cover"
+						src={item.product.image || `/images/product/${item.product.sku}.jpg`}
+						alt={item.product.name}
+						loading="lazy"
+					/>
+				</a>
+			</div>
+			<div>
+				<div class="flex items-center justify-between dark:text-slate-50">
+					<a
+						class="hover:underline"
+						href="/product/{item.product.sku}">{item.product.name}</a
+					>
+					<span>{formatPrice(item.product.price)}</span>
 				</div>
-				<div>
-					<div class="flex items-center justify-between dark:text-slate-50">
-						<a
-							class="hover:underline"
-							href="/product/{item.product.sku}">{item.product.name}</a
+				<div class="dark:text-slate-50/50">Quantity: {item.quantity}</div>
+				<!-- remove from cart -->
+				<div class="mt-4">
+					<form
+						method="post"
+						action="?/remove"
+						use:enhance={() => {
+							deleteSkus = [...deleteSkus, item.product.sku];
+							return async ({ result }) => {
+								await invalidateAll();
+								await applyAction(result);
+								deleteSkus = deleteSkus.filter((sku) => sku !== item.product.sku);
+							};
+						}}
+					>
+						<input
+							type="hidden"
+							name="sku"
+							value={item.product.sku}
+						/>
+						<button
+							class="underline disabled:text-slate-300 dark:text-slate-300 disabled:dark:text-slate-500"
+							disabled={deleteSkus.includes(item.product.sku)}
 						>
-						<span>{formatPrice(item.product.price)}</span>
-					</div>
-					<div class="dark:text-slate-50/50">Quantity: {item.quantity}</div>
-					<!-- remove from cart -->
-					<div class="mt-4">
-						<form
-							method="post"
-							action="?/remove"
-							use:enhance={() => {
-								deleteSkus = [...deleteSkus, item.product.sku];
-								return async ({ result }) => {
-									await invalidateAll();
-									await applyAction(result);
-									deleteSkus = deleteSkus.filter((sku) => sku !== item.product.sku);
-								};
-							}}
-						>
-							<input
-								type="hidden"
-								name="sku"
-								value={item.product.sku}
-							/>
-							<button
-								class="underline disabled:text-slate-300 dark:text-slate-300 disabled:dark:text-slate-500"
-								disabled={deleteSkus.includes(item.product.sku)}
-							>
-								{#if deleteSkus.includes(item.product.sku)}
-									Removing...
-								{:else}
-									Remove from bag
-								{/if}
-							</button>
-						</form>
-					</div>
+							{#if deleteSkus.includes(item.product.sku)}
+								Removing...
+							{:else}
+								Remove from bag
+							{/if}
+						</button>
+					</form>
 				</div>
 			</div>
-		{/each}
+		</div>
 	{:else}
 		<p>Cart is empty.</p>
+	{/each}
+	{#if subtotal > 0}
+		<div class="mt-12">
+			<div class="mb-1 flex items-center justify-between text-lg font-semibold text-slate-800 dark:text-slate-200">
+				<span>Subtotal</span>
+				<span>{formatPrice(subtotal)}</span>
+			</div>
+			<div class="text-slate-600 dark:text-slate-400">Shipping and taxes calculated at checkout.</div>
+			<div class="mt-6">
+				<a
+					href="/checkout"
+					class="btn block w-full">Checkout</a
+				>
+			</div>
+		</div>
 	{/if}
 </div>
