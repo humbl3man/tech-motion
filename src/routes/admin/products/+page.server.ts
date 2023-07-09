a8d51388c2ee2c2ea8f49aadb5de26a73bb08f0e
Konstantin <humbl3dev@gmail.com>
2023-07-09 14:08:52 -0700

    admin products page: add product modal WIP

--- src/routes/admin/products/+page.server.ts
+++ src/routes/admin/products/+page.server.ts
@@ -1,4 +1,23 @@
 import { db } from '$lib/db.js';
+import { fail } from '@sveltejs/kit';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import { z } from 'zod';
+
+const placeholderImage = 'https://placehold.co/1080x1080';
+
+const createProductSchema = z.object({
+	name: z
+		.string({
+			required_error: 'Product name is required'
+		})
+		.min(5, 'Product name must be at least 5 characters long'),
+	price: z
+		.number({
+			required_error: 'Product price is required'
+		})
+		.min(100, 'Product price must be at least $1'),
+	description: z.string().optional()
+});
 
 export const load = async (event) => {
 	const allProducts = await db.product.findMany({
@@ -7,7 +26,33 @@ export const load = async (event) => {
 		}
 	});
 
+	const createForm = await superValidate(event, createProductSchema);
+
 	return {
-		allProducts
+		allProducts,
+		createForm
 	};
+};
+
+export const actions = {
+	create: async (event) => {
+		const createForm = await superValidate(event, createProductSchema);
+
+		if (!createForm.valid) {
+			return fail(400, { createForm });
+		}
+
+		// TODO: logic for creating the product after validation
+
+		await db.product.create({
+			data: {
+				name: createForm.data.name,
+				price: createForm.data.price,
+				description: createForm.data.description || '',
+				image: placeholderImage
+			}
+		});
+
+		return message(createForm, 'Product created!');
+	}
 };
--- src/routes/admin/products/+page.svelte
+++ src/routes/admin/products/+page.svelte
@@ -1,9 +1,67 @@
 <script lang="ts">
+	import { Button, GradientButton, Input, Label, Modal, Textarea } from 'flowbite-svelte';
+	import { superForm } from 'sveltekit-superforms/client';
+
 	export let data;
+
+	const { form: createForm, errors, enhance, submitting } = superForm(data.createForm);
+
+	let createProductModal = false;
 	$: products = data.allProducts;
 </script>
 
-<h1 class="mb-4 text-2xl">Manage Products</h1>
+<header class="mb-12">
+	<h1 class="mb-4 text-2xl">Manage Products</h1>
+
+	<GradientButton
+		type="button"
+		color="purpleToBlue"
+		on:click={() => (createProductModal = true)}>Create new product</GradientButton
+	>
+</header>
+
+<Modal
+	bind:open={createProductModal}
+	size="xs"
+	autoclose={false}
+>
+	<h2 class="mb-4 text-xl dark:text-white">Create New Product</h2>
+	<form
+		method="post"
+		action="?/create"
+		class="flex flex-col space-y-4"
+	>
+		<Label class="space-y-2">
+			<span>Name</span>
+			<Input
+				type="text"
+				name="name"
+				required
+			/>
+		</Label>
+		<Label class="space-y-2">
+			<span>Price (in cents)</span>
+			<Input
+				type="number"
+				name="price"
+				placeholder="99999"
+				required
+			/>
+		</Label>
+		<Label class="space-y-2">
+			<span>Description</span>
+			<Textarea
+				name="description"
+				rows={4}
+			/>
+		</Label>
+		<GradientButton
+			color="purpleToBlue"
+			type="submit"
+			class="w-full">Create</GradientButton
+		>
+	</form>
+</Modal>
 
 <section class="dark:text-slate-300">
 	{#each products as product (product.sku)}
