699351a72f318e6d5712c13305834b289f3a4d30
Konstantin <humbl3dev@gmail.com>
2023-07-09 16:31:29 -0700

    admin dashboard filter admin users

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,7 +1,20 @@
+import { Role } from '$lib/constants/Role';
 import { db } from '$lib/db.js';
 
 export const load = async () => {
-	const userCount = await db.user.count();
+	const userCount = await db.user.count({
+		where: {
+			// filter by user role, only return users not admins
+			// admin cannot modify or delete other admins
+			NOT: {
+				role: {
+					is: {
+						id: Role.ADMIN
+					}
+				}
+			}
+		}
+	});
 	const productCount = await db.product.count();
 
 	return {
